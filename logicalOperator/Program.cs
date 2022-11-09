4935b0418baba6b1135e06704ed8c2b0a8c8e0f7
clon54programer <112908720+clon54programer@users.noreply.github.com>
2022-11-08 21:15:51 -0500

    Add project files.

---
+++ logicalOperator/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool value1 = true;
+bool value2 = false;
+
+//&&, ||, !
+// || si unos de los valores es true da true y si todos son false todos son false
+//deben ser  todas las condicones true para que delvuelva true
+//te delvuelve true si la condicion es false,es decir es una negacion
+
